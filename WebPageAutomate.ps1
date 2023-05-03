55357e300b644089465cce7ec4a2db4b65a6156c
prselva <131126251+prselva@users.noreply.github.com>
2023-05-03 14:04:37 +0530

    Update WebPageAutomate.ps1

--- WebPageAutomate.ps1
+++ WebPageAutomate.ps1
@@ -24,3 +24,11 @@
 
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Welcome to Cloud Operations Team from host $($env:computername) !"
+function Disable-IEESC {
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+Stop-Process -Name Explorer
+}
+Disable-IEESC
